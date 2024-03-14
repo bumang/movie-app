168942529f768f275f67f27962ec2ad535f70966
bumang <umangabhattarai11@gmail.com>
2024-03-14 14:32:08 +0545

    fix: image loader src

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
   basePath: '/movie-app',
   images: {
     loader: 'custom',
-    path: 'https://example.com/myaccount/',
+    path: '',
     remotePatterns: [
       {
         protocol: 'https',
