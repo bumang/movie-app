48ff58b5e06904369b9b332109fef2a810cfa25c
bumang <umangabhattarai11@gmail.com>
2024-03-14 16:35:25 +0545

    fix: base path next config

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   output: 'export',
-  basePath: '/' || process.env.NEXT_PUBLIC_PATH_PREFIX,
+  basePath: process.env.NEXT_PUBLIC_PATH_PREFIX,
   images: {
     unoptimized: true,
     remotePatterns: [
